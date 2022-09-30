65d3bb0bb7ffd6682e4c7c437ccb7e5368d159f5
KuuraKurta <joshuaespinal9401@gmail.com>
2022-09-29 23:06:27 -0400

    added special character prompt

--- script.js
+++ script.js
@@ -34,7 +34,14 @@ function generatePassword() {
   } else if
     (isPasswordUppercase == "n"){
       isPasswordUppercase = false
-    }  
+    }
+  var isPasswordSpecial = prompt ("Do you want special characters in your password? Please input either y or n");
+  if (isPasswordSpecial == "y"){
+    isPasswordSpecial = true
+  } else if
+    (isPasswordSpecial == "n"){
+      isPasswordSpecial = false
+    }    
   // Data
   // we need different sets of characters - maybe strings or arrays
     // numbers
